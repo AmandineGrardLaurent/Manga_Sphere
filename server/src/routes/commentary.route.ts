39cbcaa20730bfcf6aaf12e4887a07e108bb8831
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-02-13 11:34:15 +0100

    feat: commentaries

--- server/src/routes/commentary.route.ts
+++ server/src/routes/commentary.route.ts
@@ -1,11 +1,8 @@
-// import express from "express";
+import express from "express";
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.get(
-//   "/api/serie/commentary/:id"
+router.get("/api/serie/commentary/:id");
+router.post("/api/serie/commentary");
 
-// );
-// router.post("/api/serie/commentary");
-
-// export default router;
+export default router;
