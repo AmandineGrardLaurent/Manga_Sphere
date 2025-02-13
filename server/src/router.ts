92b12d81d6b28f182fc03fb2c96e897f4ee034bb
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-02-13 11:41:43 +0100

    feat: commentaries

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-// import CommentaryRoute from "./routes/commentary.route";
+import CommentRoute from "./routes/comment.route";
 import SeasonRoute from "./routes/season.route";
 import SerieRoute from "./routes/serie.route";
 import UserRoute from "./routes/user.route";
@@ -11,6 +11,6 @@ router.use("/", UserRoute);
 router.use("/", SerieRoute);
 router.use("/", SeasonRoute);
 router.use("/", VolumeRoute);
-// router.use("/", CommentaryRoute);
+router.use("/", CommentRoute);
 
 export default router;
--- server/src/routes/commentary.route.ts
+++
@@ -1,8 +0,0 @@
-// import express from "express";
-
-// const router = express.Router();
-
-// router.get("/api/serie/commentary/:id");
-// router.post("/api/serie/commentary");
-
-// export default router;
